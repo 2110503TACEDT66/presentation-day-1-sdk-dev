27afe52d00205001107c00e781a1152a430bc271
SIER101 <73215103+SIER101@users.noreply.github.com>
2024-03-04 17:35:53 +0700

    fix response status

--- models/Booking.js
+++ models/Booking.js
@@ -1,24 +1,30 @@
 const mongoose = require("mongoose");
-const BookingSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  hotel: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Hotel",
+const BookingSchema = new mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    hotel: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Hotel",
 
-    required: true,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 module.exports = mongoose.model("Booking", BookingSchema);
